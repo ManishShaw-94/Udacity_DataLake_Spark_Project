c2b65bd06bbb16fe1f0cce30eb44c39291e1a8dc
Manish Shaw <66125159+ManishShaw-94@users.noreply.github.com>
2023-03-14 01:02:37 +1100

    Add files via upload

---
+++ customer_landing.sql
@@ -0,0 +1,15 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `manish`.`customer_landing` (
+  `customerName` string,
+  `email` string,
+  `phone` string,
+  `birthDay` string,
+  `serialNumber` string,
+  `registrationDate` bigint,
+  `lastUpdateDate` bigint,
+  `shareWithResearchAsOfDate` bigint,
+  `shareWithPublicAsOfDate` bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://manish-s3-lake-house/customer/landing/'
+TBLPROPERTIES ('classification' = 'json');
